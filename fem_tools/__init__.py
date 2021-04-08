16673b40cbe2872eeab3460d76fead71bd48db1a
ogurechik <rav-navini-gego-cutropal@yandex.ru>
2021-04-08 13:35:11 +0300

    Добавили пакет интсрументов по МКЭ

---
+++ fem_tools/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+fem_tools - пакет предоставляющий полезные функции и классы
+для метода конечных элементов (МКЭ)
+"""
+from . import structure
+from . import calc
+
+__all__ = ['calc', 'structure']
