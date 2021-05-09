d4138e7ed1d3a3c0a2861ec6745c7cf5adc1a000
Alisa11110000 <79804290+Alisa11110000@users.noreply.github.com>
2021-05-09 14:40:27 +0300

    Добавили тесты на умножение

--- test/matan_tests.py
+++ test/matan_tests.py
@@ -61,6 +61,77 @@ class TestMatrixOperations(unittest.TestCase):
             # строку и столбец нельзя складывать
             row + col
 
+    def test_mult_row_and_col(self):
+        """Умножение строки на столбец"""
+        row = Matrix([1, 2], to_row=True)
+        col = Matrix([3, 4])
+        res = Matrix([11])
+        msg = f"\n{row * col} НЕ РАВНО {res}"
+        self.assertEqual(row * col, res, msg)
+
+    def test_mult_col_and_row(self):
+        """Умножение столбца на строку"""
+        col = Matrix([1, 2])
+        row = Matrix([3, 4], to_row=True)
+        res = Matrix([[3, 4], [6, 8]])
+        msg = f"\n{col * row} НЕ РАВНО\n{res}"
+        self.assertEqual(col * row, res, msg)
+
+    def test_mult_cols(self):
+        """Умножение столбца на столбец"""
+        col = Matrix([1, 2])
+        with self.assertRaises(Exception):
+            # столбец нельзя умножить на столбецц
+            # Неподходящие размерности
+            col * col
+
+    def test_mult_rows(self):
+        """Умножение столбца на столбец"""
+        row = Matrix([1, 2], to_row=True)
+        with self.assertRaises(Exception):
+            # строку нельзя умножить на строку
+            # Неподходящие размерности
+            row * row
+
+    def test_mult_bad_matrix(self):
+        """Умножить не соразмерные матрицы"""
+        A = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        B = Matrix([[1, 2], [3, 4]])
+        with self.assertRaises(Exception):
+            # несоразмерные матрицы нельзя умножать
+            A * B
+
+    def test_mult_good_matrix(self):
+        """Умножение соразмерных матриц"""
+        A = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        res = Matrix([[30, 36, 42], [66, 81, 96], [102, 126, 150]])
+        msg = f"\n{A * A} НЕ РАВНО\n{res}"
+        self.assertEqual(A * A, res, msg)
+
+    def test_mult_col_and_num(self):
+        """Умножение столбца на число"""
+        col = Matrix([1, 2, 3])
+        num = 2
+        res = Matrix([2, 4, 6])
+        msg = f"\n{col}умножить на {num} НЕ РАВНО\n{res}"
+        self.assertEqual(col * num, res, msg)
+
+    def test_mult_row_and_num(self):
+        """Умножение строки на число"""
+        row = Matrix([1, 2, 3], to_row=True)
+        num = 2
+        res = Matrix([2, 4, 6], to_row=True)
+        msg = f"\n{row}умножить на {num} НЕ РАВНО {res}"
+        self.assertEqual(row * num, res, msg)
+
+    def test_mult_matrix_and_num(self):
+        """Умножение матрицы на число"""
+        m = Matrix([[1, 2], [3, 4]])
+        num = 2
+        res = Matrix([[2, 4], [6, 8]])
+        msg = f"\n{m}умножить на {num} НЕ РАВНО\n{res}"
+        self.assertEqual(m * num, res, msg)
+
 
 if __name__ == '__main__':
     unittest.main()
